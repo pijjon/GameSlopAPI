179c115deb8469b063d02d4da0ffc79982ff807b
Alwin Zhao <alwinzhao12@gmail.com>
2025-12-19 04:13:39 -0500

    remove unnecessary imports

--- src/main/java/org/yearup/controllers/ShoppingCartController.java
+++ src/main/java/org/yearup/controllers/ShoppingCartController.java
@@ -1,14 +1,14 @@
 package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import org.yearup.data.ProductDao;
 import org.yearup.data.ShoppingCartDao;
 import org.yearup.data.UserDao;
-import org.yearup.models.Product;
+
 import org.yearup.models.ShoppingCart;
 import org.yearup.models.User;
 
