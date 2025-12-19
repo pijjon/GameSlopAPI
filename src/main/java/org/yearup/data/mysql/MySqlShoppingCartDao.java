c645c1e8bd39a5affe6e88ba0045add7d4bce8b7
Alwin Zhao <alwinzhao12@gmail.com>
2025-12-19 02:28:05 -0500

    create MySqlShoppingCartDao file that extends datasource and implements ShoppingCartDao interface

---
+++ src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -0,0 +1,20 @@
+package org.yearup.data.mysql;
+
+import org.springframework.stereotype.Component;
+import org.yearup.data.ShoppingCartDao;
+import org.yearup.models.ShoppingCart;
+
+import javax.sql.DataSource;
+
+@Component
+public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDao {
+
+    public MySqlShoppingCartDao(DataSource dataSource) {
+        super(dataSource);
+    }
+
+    @Override
+    public ShoppingCart getByUserId(int userId) {
+        return null;
+    }
+}
