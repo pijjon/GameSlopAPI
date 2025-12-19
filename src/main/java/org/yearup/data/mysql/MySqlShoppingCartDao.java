e109387fbbc561ecb67d07225db28e878f6ae6b8
Alwin Zhao <alwinzhao12@gmail.com>
2025-12-19 03:08:10 -0500

    create MySqlShoppingCartDao.mapRow() method

--- src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlShoppingCartDao.java
@@ -17,4 +17,27 @@ public class MySqlShoppingCartDao extends MySqlDaoBase implements ShoppingCartDa
     public ShoppingCart getByUserId(int userId) {
         return null;
     }
+
+    private ShoppingCart mapRow(ResultSet row) throws SQLException {
+        ShoppingCart cart = new ShoppingCart();
+
+        int productId = row.getInt("product_id");
+        int quantity = row.getInt("quantity");
+        String name = row.getString("name");
+        BigDecimal price = row.getBigDecimal("price");
+        int categoryId = row.getInt("category_id");
+        String description = row.getString("description");
+        String subCategory = row.getString("subcategory");
+        String imageUrl = row.getString("image_url");
+        int stock = row.getInt("stock");
+        boolean isFeatured = row.getBoolean("featured");
+
+        ShoppingCartItem item = new ShoppingCartItem();
+        item.setProduct(new Product(productId, name, price, categoryId, description, subCategory, stock, isFeatured, imageUrl));
+        item.setQuantity(quantity);
+        cart.add(item);
+
+
+        return cart;
+    }
 }
